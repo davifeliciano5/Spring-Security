6a10d4da56696d5a82ff5ae4e4cbbf0076793808
davifeliciano5 <davifelicianosousa2020@gmail.com>
2025-11-04 02:43:35 -0300

    Adicionando classe médico

---
+++ Curso spring security 1/3865-seguranca-java-aula-01/src/main/java/med/voll/web_application/domain/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package med.voll.web_application.domain;
+
+public class RegraDeNegocioException extends RuntimeException{
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
---
+++ Curso spring security 1/3865-seguranca-java-aula-01/src/main/java/med/voll/web_application/domain/medico/Especialidade.java
@@ -0,0 +1,8 @@
+package med.voll.web_application.domain.medico;
+
+public enum Especialidade {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA
+}
