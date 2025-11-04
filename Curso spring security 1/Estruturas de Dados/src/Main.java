c9b581ea51506f4f1e5751fad737de9312cd2d6d
davifeliciano5 <davifelicianosousa2020@gmail.com>
2025-11-04 00:39:26 -0300

    a

---
+++ Curso spring security 1/Estruturas de Dados/src/Main.java
@@ -0,0 +1,14 @@
+import Vetores.Vetores;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Vetores vetor = new Vetores(5);
+
+
+            vetor.adiciona("Elemento num 1");
+            vetor.adiciona("Elemento num 2");
+
+    }
+}
